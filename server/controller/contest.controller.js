7274ea6a35fac15087641c5dd398b16542c3779b
peterimoh <peterimoh2@gmail.com>
2021-11-28 23:40:59 +0000

    env

--- server/controller/contest.controller.js
+++ server/controller/contest.controller.js
@@ -1,29 +1,28 @@
-const Voter = require('./vote.controller')
+const Voter = require('./vote.controller');
 
 const Model = require('../model/auth.model');
 
 class Contest extends Voter {
+  res;
+  constructor(res) {
+    this.res = res;
+  }
 
-    res;
-    constructor(res){
-        this.res = res;
-    }
-
-    Create(contestObject, contest) {
-
-        Model.Insert(contest, contestObject, (err, result)=>{
-            if(err) this.TransactionMessage(res, 400, "Error creating contest table")
-            else {
-                Model.Select(contest, (err, result)=>{
-                    if(err) this.TransactionMessage(res, 400, "Error getting contest table");
-                    else{
-                        this.TransactionMessage(res, 200, result)
-                    }
-                })
-            }
-        })
-    }
-
+  Create(contest, contestObject) {
+    Model.Insert(contest, contestObject, (err, result) => {
+      if (err)
+        this.TransactionMessage(res, 400, 'Error creating contest table');
+      else {
+        Model.Select(contest, (err, result) => {
+          if (err)
+            this.TransactionMessage(res, 400, 'Error getting contest table');
+          else {
+            this.TransactionMessage(res, 200, result);
+          }
+        });
+      }
+    });
+  }
 }
 
 module.exports = Contest;
